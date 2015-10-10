cbbae2d9c2c1f303d96d929b2a2c4e839eb5270e
djironimo <dietzvv@gmail.com>
2015-10-10 18:22:42 +0600

    added support for 3 groups of files: images, audio, video; and error for others

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	var template = $("#media-template").html();
+	var	compiled_html = _.template(template)({
+			data: readJson().array
+		});
+	$("body").append(compiled_html);
+
+});
+
+var imageID="#image-template";
+	audioID="#audio-template";
+	videoID="#video-template";
+	errorID="#error-template";
+
+function showTemplate (data) {
+	if (data.type == "image") {
+		return mediaTemplate (data.name, imageID);
+	} else if (data.type == "audio") {
+		return mediaTemplate (data.name, audioID);
+	} else if (data.type == "video") {
+		return mediaTemplate (data.name, videoID);
+	} else {
+		return mediaTemplate (data.name, errorID);
+	}
+}
+
+function mediaTemplate (name, id) {
+	var temp = $(id).html();
+	var compiled = _.template(temp)({
+		data: name
+	});
+	return compiled;
+}
+
+function getIcon (data) {
+	if (data.type == "image") {
+		return "<i class='fa fa-file-image-o ico'></i>";
+	} else if (data.type == "audio") {
+		return "<i class='fa fa-file-audio-o ico'></i>";
+	} else if (data.type == "video") {
+		return "<i class='fa fa-file-video-o ico'></i>";
+	} else {
+		return "<i class='fa fa-file-o ico'></i>";
+	}
+}
---
+++ readJson.js
@@ -0,0 +1,33 @@
+function readJson () {
+    json =  {
+            "array": [          
+                {
+                  "name": "picture_1.jpg",
+                  "type": "image"
+                },
+                {
+                  "name": "picture_2.png",
+                  "type": "image"
+                },
+                {
+                  "name": "picture_3.jpg",
+                  "type": "image"
+                },
+                {
+                  "name": "06 old devil.mp3",
+                  "type": "audio"
+                },
+                {
+                  "name": "text_1",
+                  "type": "other"
+                },
+                {
+                    "name": "video_1.mp4",
+                    "type": "video"
+                }
+            ]
+        };
+
+    return json;
+}
+
